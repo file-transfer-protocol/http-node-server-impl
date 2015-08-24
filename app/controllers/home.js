1659c9363150ceae6e231a75a19887d62c62735b
Vaisakh Babu <vaisakh.babu@entapza.com>
2015-08-24 22:20:17 +0530

    removed unwanted dependencies

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,18 +1,16 @@
 var express = require('express');
 var router = express.Router();
-var mongoose = require('mongoose');
-var Article = mongoose.model('Article');
+//var mongoose = require('mongoose');
+//var Article = mongoose.model('Article');
 
 module.exports = function (app) {
     app.use('/', router);
-};
 
-router.get('/', function (req, res, next) {
-    Article.find(function (err, articles) {
-        if (err) return next(err);
-        res.render('index', {
-            title: 'Generator-Express MVC',
-            articles: articles
-        });
-    });
-});
+    var index = function (req, res, next) {
+        var data = '<h1>hello world</h1>';
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+    }
+
+    router.get('/', index);
+};
